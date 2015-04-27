5e11772de0e7c96ac58df27a42a868d2a85c01f0
kurid <kurid11@freeuni.edu.ge>
2015-04-27 23:53:36 +0400

    fixed test error

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,12 +1,11 @@
 import static org.junit.Assert.*;
 
+import org.mockito.Mockito;
+
 
 public class Test {
 	SimpleProgram sim = new SimpleProgram();
-	@org.junit.Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	SimpleProgram simMockito = Mockito.mock(SimpleProgram.class);
 
 	public void SumTest(){
 		int i = 23;
@@ -18,6 +17,20 @@ public class Test {
 		int i = 23;
 		int j = 2;
 		assertEquals(sim.multiply(i, j),i*j);
+	}
+	
+	public void SumTestMockito(){
+		int i = 23;
+		int j = 2;
+
+		Mockito.when(simMockito.multiply(i, j)).thenReturn(i*j);
+	}
+	
+	public void mulTestMockito(){
+		int i = 23;
+		int j = 2;
+
+		Mockito.when(simMockito.sum(i, j)).thenReturn(i+j);
 	}
 
 }
