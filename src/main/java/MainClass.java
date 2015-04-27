4b7c2991cceb9bc699c8a0c08ab5754ad6b64c90
kurid <kurid11@freeuni.edu.ge>
2015-04-27 23:28:30 +0400

    simple program added

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,9 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		SimpleProgram s = new SimpleProgram();
+		System.out.println(s.sum(1,2));
+		System.out.println(s.multiply(3,5));
+	}
+}
---
+++ src/main/java/SimpleProgram.java
@@ -0,0 +1,11 @@
+
+public class SimpleProgram {
+
+	public static int multiply(int i, int j) {
+		return i*j;
+	}
+
+	public static int sum(int i, int j) {
+		return i+j;
+	}
+}
